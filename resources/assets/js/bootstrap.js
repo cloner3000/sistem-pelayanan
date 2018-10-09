0032ac8b8eb91b084f2e936f49987a99d092d2ca
dekiakbar <dekiakbar@linuxmail.org>
2018-10-09 23:30:20 +0700

    tambah admin vie

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -13,6 +13,13 @@ try {
     require('bootstrap-sass');
 } catch (e) {}
 
+//admin LTE
+require('admin-lte');
+
+//chart.js
+require('chart.js');
+
+
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
@@ -37,19 +44,4 @@ if (token) {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
 
-// import Echo from 'laravel-echo'
-
-// window.Pusher = require('pusher-js');
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: 'your-pusher-key',
-//     cluster: 'mt1',
-//     encrypted: true
-// });
