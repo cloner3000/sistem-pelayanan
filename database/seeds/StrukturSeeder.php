d29a344ffa55336106577193b244f5c1d67c6efc
dekiakbar <dekiakbar@linuxmail.org>
2018-10-23 00:04:15 +0700

    domPDF,seeder,view forget to add untracked file -_-

---
+++ database/seeds/StrukturSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class StrukturSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $f 		= Faker\factory::create('id_ID');
+        $limit 	= 12;
+        $jabatan = array('Kepala Desa','Sekertaris','Bendahara');
+        for ($i=0; $i < $limit; $i++) { 
+        	DB::table('strukturs')->insert([
+				'nama' => $f->name,
+				'jabatan' => $f->randomElement($jabatan),
+				'foto' => "foto",
+				'fb' => $f->word,
+				'twitter' => $f->word,
+        	]);
+        }
+    }
+}
---
+++ database/seeds/WebSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class WebSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        DB::table('webs')->insert([
+			'nama_website'      => "Contoh",
+			'judul_slider'      => "Contoh judul",
+			'deskripsi_slider'  => "Blablablablablablablablabla",
+			'foto_slider'       => "1540040137.jpg",
+			'judul_slider1'     => "Contoh judul",
+			'deskripsi_slider1' => "Blablablablablablablablabla",
+			'foto_slider1'      => "1540040138.jpg",
+			'tentang'           => "BlablablablablablablablablaBlablablablablablablablablaBlablablablablablablablabla",
+			'visi_misi'         => "BlablablablablablablablablaBlablablablablablablablablaBlablablablablablablablabla",
+			'tlp'               => "099999999999",
+			'email'             => "admin@admin.com",
+			'fb'                => "contoh",
+			'twitter'           => "contoh",
+			'ig'                => "contoh",
+        ]);
+    }
+}
