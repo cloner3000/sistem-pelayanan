9d07e7d3aa1b2ff91990cddb601d408446a35225
dekiakbar <dekiakbar@linuxmail.org>
2018-10-24 04:17:12 +0700

    web controller

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -5,12 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Web;
 use App\Struktur;
+use App\Ktp;
+use App\Skk;
+use App\Sptjm;
+use App\Spp;
 class WebController extends Controller
 {
     public function index()
     {
     	$web = Web::firstOrFail();
     	$strukturs = Struktur::all();
-    	return view('welcome1',compact('web','strukturs'));
+    	$ktp = Ktp::count();
+    	$skk = Skk::count();
+    	$sptjm = Sptjm::count();
+    	$spp = Spp::count();
+    	return view('welcome1',compact('web','strukturs','ktp','skk','sptjm','spp'));
     }
 }
