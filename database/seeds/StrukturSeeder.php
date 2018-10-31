3413bfab3ae3b4dca78b31f6b75f69b42b69c34e
dekiakbar <dekiakbar@linuxmail.org>
2018-10-31 19:56:18 +0700

    fix struktur

--- database/seeds/StrukturSeeder.php
+++ database/seeds/StrukturSeeder.php
@@ -12,22 +12,48 @@ class StrukturSeeder extends Seeder
     public function run()
     {
         $nama = array(
-            'Hilmi Nurhikmat','Fahmi pamungkas','Iwan R. Maulana','Cici Endas Rahmatika, A.Md','Hastiti Tresna Ayu',
-            'Dudin Saepudin','Beni Lesmana, S.IP','Dede Abdillah, S.HI','Isep Saepul Rohman',
-            'Handry Sulistyio H','Kamal Chairil','Ahmad Nurzaman'
+            'Hilmi Nurhikmat',
+            'Beni Lesmana, S.IP',
+            'Fahmi pamungkas',
+            'Iwan R. Maulana',
+            'Cici Endas Rahmatika, A.Md',
+            'Hastiti Tresna Ayu',
+            'Dudin Saepudin',
+            'Dede Abdillah, S.HI',
+            'Isep Saepul Rohman',
+            'Handry Sulistyio H',
+            'Kamal Chairil',
+            'Ahmad Nurzaman'
         );
 
         $foto = array(
-            '1540989182.JPG','1540934634.jpeg','1540934671.jpeg','1540934683.jpeg','1540934702.jpeg','1540934717.jpeg',
-            '1540934726.jpeg','1540934649.jpeg','1540971179.jpeg','1540971190.jpeg','1540971200.jpeg',
+            '1540989182.JPG',
+            '1540934726.jpeg',
+            '1540934634.jpeg',
+            '1540934671.jpeg',
+            '1540934683.jpeg',
+            '1540934702.jpeg',
+            '1540934717.jpeg',
+            '1540934649.jpeg',
+            '1540971179.jpeg',
+            '1540971190.jpeg',
+            '1540971200.jpeg',
             '1540971210.jpeg'
         );
         
         $jabatan = array(
-            'Kepala Desa','Kepala Urusan perencanaan','Kepala Dusun Malinggut 1','Kepala Urusan Administrasi Umum dan Tata Usaha',
-            'Kepala Urusan Keuangan / Bendahara Desa','Kepala dusun Malinggut 2','Sekretaris Desa','Kepala Seksi Pelayanan',
-            'Kepala Dusun Malinggut 3','Kepala Seksi Pemerintahan','Kepala Dusun Sukamaju','Kasi Kesejahteraan'
-
+            'Kepala Desa',
+            'Sekretaris Desa',
+            'Kepala Urusan perencanaan',
+            'Kepala Dusun Malinggut 1',
+            'Kepala Urusan Administrasi Umum dan Tata Usaha',
+            'Kepala Urusan Keuangan',
+            'Kepala dusun Malinggut 2',
+            'Kepala Seksi Pelayanan',
+            'Kepala Dusun Malinggut 3',
+            'Kepala Seksi Pemerintahan',
+            'Kepala Dusun Sukamaju',
+            'Kasi Kesejahteraan'
         );
 
         foreach ($nama as $key => $n) {
